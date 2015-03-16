d2c4918a21e22bc84e3948be7d7fa6f533a9401e
qiwsir <qiwsir@gmail.com>
2015-03-16 20:01:33 +0800

    string 2

---
+++ 1code/107.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+name = raw_input("What is your name?")
+age = raw_input("How old are you?")
+
+print "Your name is:", name
+print "You are " + age + " years old."
+
+after_ten = int(age) + 10
+print "You will be " + str(after_ten) + " years old after ten years."

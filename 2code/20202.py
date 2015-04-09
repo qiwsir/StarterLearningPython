5c7fe6c2e0e34137fecde139fcfcb13b0ac7d077
qiwsir <qiwsir@gmail.com>
2015-04-09 23:22:02 +0800

    function 2

---
+++ 2code/20202.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def fibs(n):
+    """
+    This is a Fibonacci sequence.
+    """
+    result = [0,1]
+    for i in range(n-2):
+        result.append(result[-2] + result[-1])
+    return result
+
+if __name__ == "__main__":
+    #lst = fibs(10)
+    #print lst
+    fibs(10)

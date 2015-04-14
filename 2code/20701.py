b05010129420eb468ff77386434a002dcc74a09f
qiwsir <qiwsir@gmail.com>
2015-04-14 12:23:55 +0800

    clsss 3

--- 2code/20701.py
+++ 2code/20701.py
@@ -6,19 +6,25 @@ __metaclass__ = type
 class Person:
     def __init__(self, name):
         self.name = name
-        print self
-        print type(self)
 
     def getName(self):
-        return self.name
-
+        #return self.name
+        return girl.name
+    
+    def breast(self, n):
+        self.breast = n
+        
     def color(self, color):
         print "%s is %s" % (self.name, color)
 
+    def how(self):
+        print "%s breast is %s" % (self.name, self.breast)
+
 girl = Person('canglaoshi')
 name = girl.getName()
-print "the person's name is: ", name
-girl.color("white")
+print name
 
-print "------"
-print girl.name
+girl.breast(90)
+
+girl.color("white")
+girl.how()

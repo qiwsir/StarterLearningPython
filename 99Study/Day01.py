e46dd38661687255dab4618ad4d676a7159a5eae
Cuiyansong <229063661@qq.com>
2015-10-22 22:21:31 +0800

    D01-20151022
     - Add Day01.py
     - Update typo in 106.md

---
+++ 99Study/Day01.py
@@ -0,0 +1,12 @@
+import math
+
+print "Day 1"
+print "Hello, world"
+print type(33333)
+print type(33.222)
+print type(math.pow)
+print 'What\'s your name?' == "What's your name?"
+a = 1
+b = "2"
+print `a` + b == "12"
+print '\t' + '\v'

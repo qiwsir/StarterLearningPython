2dc5fad6bc9213ce5a065410716b05d032170fe9
qiwsir <qiwsir@gmail.com>
2015-11-03 18:14:43 +0800

    module and package

---
+++ 2code/21901.py
@@ -0,0 +1,11 @@
+# /usr/bin/env python
+# coding:utf-8
+
+public_variable = "Hello, I am a public variable."
+_private_variable = "Hi, I am a private variable."
+
+def public_teacher():
+    print "I am a public teacher, I am from JP."
+
+def _private_teacher():
+    print "I am a private teacher, I am from CN."

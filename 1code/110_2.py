88851a48b9d3e3d7df0e06635893f87eb410b378
老齐 <qiwsir@gmail.com>
2016-03-24 11:59:22 +0800

    p3

---
+++ 1code/110_2.py
@@ -0,0 +1,8 @@
+# -*- coding: cp936 -*-
+#python 2
+
+import sys
+
+print sys.getdefaultencoding()
+
+print "《跟老齐学python》值得读"
---
+++ 1code/110_3.py
@@ -0,0 +1,7 @@
+#python 3
+
+import sys
+
+print(sys.getdefaultencoding())
+
+print("《跟老齐学python》值得读")

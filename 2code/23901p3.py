9e10dc516d8c8e7d3a1af432c7847725012b59f4
老齐 <qiwsir@gmail.com>
2016-04-20 09:20:45 +0800

    p3

---
+++ 2code/23901p3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+class RoundFloat:
+    def __init__(self, val):
+        assert isinstance(val, float), "value must be a float."
+        self.value = round(val, 2)
+
+    def __str__(self):
+        return "{:.2f}".format(self.value)
+        
+    __repr__ = __str__
+
+    #def __repr__(self):
+    #    return self.__str__()
+    
+if __name__ == "__main__":
+    r = RoundFloat(2.0)
+    print(r)
+    print(type(r))

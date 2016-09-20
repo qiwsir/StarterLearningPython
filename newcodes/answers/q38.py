b0036093266ac0cdf811b3a45aa58bd902792cb8
qiwsir <qiwsir@gmail.com>
2016-09-20 12:01:23 +0800

    check email

---
+++ newcodes/answers/q38.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import re
+
+def validate_email(email):
+    if len(email) > 4:
+        if re.match("^[a-zA-Z0-9._%-]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$", email) != None:
+            return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    while True:
+        e = input("Input your email('q'-exit):")
+        if e == "q":
+            print("Bye!")
+            break
+        else:
+            r = validate_email(e)
+            if r:
+                print("The email is right.")
+            else:
+                print("sorry, the email is not right.")

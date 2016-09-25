0dd713c0c0c571d67d347bf8bb3c8161243bbaf2
qiwsir <qiwsir@gmail.com>
2016-09-25 21:49:05 +0800

    replace word

--- newcodes/answers/q70.py
+++ newcodes/answers/q70.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-class Keeper:
-    def __init__(self, keep):
-        self.keep = keep
-    def __getitem__(self, s):
-        if s not in self.keep:
-            return None
-        return s
-    def __call__(self, s):
-        return s.translate(self)
+import re
+
+class make_xlat:
+    def __init__(self, *args, **kwargs):
+        self.adict = dict(*args, **kwargs)
+        self.rx = self.make_rx()
+
+    def make_rx(self):
+        return re.compile('|'.join(map(re.escape, self.adict)))
+
+    def one_xlat(self, match):
+        return self.adict[match.group(0)]
+
+    def __call__(self, text):
+        return self.rx.sub(self.one_xlat, text)
 
 if __name__ == "__main__":
-    mf = Keeper
-    cang = mf('canglaoshi')
-    print(cang("what is your name? my name is laoshicang"))
+    text = "PHP is the best language"
+    adict = {"PHP": "Canglaoshi", "language":"teacher",}
+    t = make_xlat(adict)
+    print(text)
+    print("--->")
+    print(t(text))

1ce12e777acd3f40c0341dc83277ff0a2df5c651
vikash <beastthermal@gmail.com>
2020-11-12 00:39:34 +0530

    comments were added for better understanding.

--- python3code/chapter02/forelse.py
+++ python3code/chapter02/forelse.py
@@ -4,12 +4,15 @@ filename: forelse.py
 '''
 
 from math import sqrt
+"""
+math is module/ library under python which  can perform simple math operations.
+"""
 
-for n in range(99, 1, -1):
-    root = sqrt(n)
-    if root == int(root):
+for n in range(99, 1, -1): # we are iterating the for loop starting from 99 with a diffrence of 1 and ends at 10 (including).
+    root = sqrt(n) # this will calculate the square root of n 
+    if root == int(root):# we are checking data type
         print(n)
-        break
+        break # if condition not satisfy it will move out of loop
 
 else:
-    print("Nothing.")
+    print("Nothing.") # when above execution  condition fails.
